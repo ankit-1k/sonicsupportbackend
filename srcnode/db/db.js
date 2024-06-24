4e6083e6dfa296d7163fbfea91c10b655cf0deb9
ankit-1k <ankitpanda922@gmail.com>
2024-06-24 17:22:26 +0530

    mongo url

--- srcnode/db/db.js
+++ srcnode/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect("mongodb+srv://sonicdev:F_*E5Ja98i$$2q3@cluster0.r5nc91a.mongodb.net/sonicsupport");
         console.log('DB connected');
     } catch (error) {
         console.error('Failed to connect to DB:', error);
