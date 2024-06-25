95ea047823ef695d2b1712f032a0234ff1c5c98d
ankit-1k <ankitpanda922@gmail.com>
2024-06-25 06:41:04 +0530

    with error

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ connectDB();
 
 // Use routes
 app.use(appModalRoutes);
-app.use(express.static(path.join(__dirname, 'build')));
 
-// Serve the React app on any other route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
