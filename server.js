c509068e5781b6f7b8c3536c5a831bdba1bfaa24
ankit-1k <ankitpanda922@gmail.com>
2024-06-25 10:57:07 +0530

    retry

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(bodyParser.json());
 
 // Connect to DB
 connectDB();
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 // Use routes
 app.use(appModalRoutes);
 
